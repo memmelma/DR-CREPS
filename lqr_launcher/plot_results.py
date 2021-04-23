c9f0122bca56fd6ac2ce22c8b423eefb8de2cb2b
memmelma <marius.memmel@gmx.net>
2021-04-23 10:57:34 +0200

    added KL plotting

--- lqr_launcher/plot_results.py
+++ lqr_launcher/plot_results.py
@@ -14,6 +14,7 @@ def load_data_from_dir(data_dir):
     gain_policy_all = []
     mi_avg_all = []
     mus_all = []
+    kls_all = []
     legend = []
     optimal_reward_all = []
     best_reward_all = []
@@ -30,6 +31,7 @@ def load_data_from_dir(data_dir):
             init_params = None
             mi_avg = None
             mus = None
+            kls = None
             optimal_reward = None
             best_reward = None
 
@@ -45,12 +47,6 @@ def load_data_from_dir(data_dir):
                     # print(f"{data['alg'].__name__} - optimal: {data['optimal_reward']} - best: {data['best_reward']})")
 
                     if returns_mean is None:
-
-                        try:
-                            print('MIN KL', min(data['kls']))
-                            print('MAX KL', max(data['kls']))
-                        except:
-                            pass
 
                         print(data['alg'].__name__, data.keys())
                         print(data['ineff_params'])
@@ -69,6 +65,7 @@ def load_data_from_dir(data_dir):
                             mi_avg = [data['mi_avg']]
 
                         mus = [data['mus']]
+                        kls = [data['kls']]
 
                     else:
                         returns_mean += [data['returns_mean']]
@@ -84,7 +81,8 @@ def load_data_from_dir(data_dir):
                             mi_avg += [data['mi_avg']]
 
                         mus += [data['mus']]
-                        
+                        kls += [data['kls']]
+
                     file_count += 1
             
             # returns_mean = returns_mean / file_count
@@ -100,10 +98,11 @@ def load_data_from_dir(data_dir):
             gain_policy_all += [gain_policy]
             mi_avg_all += [mi_avg]
             mus_all += [mus]
+            kls_all += [kls]
             optimal_reward_all += [optimal_reward]
             best_reward_all += [best_reward]
 
-    return returns_mean_all, returns_std_all, optimal_reward_all, best_reward_all, mi_avg_all, mus_all, legend, init_params
+    return returns_mean_all, returns_std_all, optimal_reward_all, best_reward_all, mi_avg_all, mus_all, kls_all, legend, init_params
 
 def load_data_from_dir_segway(data_dir):
 
@@ -200,6 +199,8 @@ def plot_data(returns_mean_all, returns_std_all, optimal_reward_all, legend, exp
 
     fig, ax = plt.subplots()
     
+    colors = ['tab:blue','tab:orange','tab:green','tab:purple','tab:brown','tab:pink','tab:gray','tab:olive','tab:cyan']
+
     for i in range(len(returns_mean_all)):
         y = np.array(returns_mean_all[i]).mean(axis=0)
         x = np.arange(0, y.shape[0], 1)
@@ -209,8 +210,8 @@ def plot_data(returns_mean_all, returns_std_all, optimal_reward_all, legend, exp
         # print(np.array(returns_mean_all[i])[:,0].shape)
         # print(np.array(returns_mean_all[i])[:,0].mean(), np.array(returns_mean_all[i])[:,0].std())
 
-        ax.plot(x,y, label=legend[i])
-        ax.fill_between(x, (y-ci), (y+ci), alpha=.3)
+        ax.plot(x,y, label=legend[i])#, color=colors[i])
+        ax.fill_between(x, (y-ci), (y+ci), alpha=.3)#, color=colors[i])
         # ax.errorbar(x, y, yerr=ci, errorevery=10)
 
     # ax.set_ylim(-1000,0)
@@ -224,7 +225,7 @@ def plot_data(returns_mean_all, returns_std_all, optimal_reward_all, legend, exp
     plt.legend()
     # plt.savefig(f'imgs/{exp_name}/{exp_name}.pdf')
 
-    plt.title(f'samples {config["ep_per_fit"]} w/ {legend}')
+    plt.title(f'samples {config["ep_per_fit"]} lqr_dim {init_params["lqr_dim"]} n_ineff {init_params["n_ineff"]}')
     plt.savefig(f'imgs/{exp_name}/{exp_name}.png')
 
 def plot_mi(avg_mi_all, mi_idx, exp_name, config):
@@ -289,13 +290,16 @@ def plot_mu_diff(mus_all, exp_name, config):
 
     mus_all = np.array(mus_all)
     mus_all = np.diff(mus_all, axis=2, prepend=0)[:,:,:n_epochs,:]
+    mus_all = np.abs(mus_all)
 
     os.makedirs(f'imgs/{exp_name}', exist_ok=True)
     
     fig, ax = plt.subplots()
     
-    y = mus_all.mean(axis=1)
-    
+    # print(mus_all.shape)
+    y = mus_all[:,:,:,60:89].mean(axis=1)
+    # y = mus_all.mean(axis=1)
+
     x = np.arange(0, y.shape[1], 1)/config['fit_per_epoch']
     ci = mus_all.std(axis=1)*2
 
@@ -342,18 +346,40 @@ def plot_mu_diff(mus_all, exp_name, config):
         plt.legend(legend)
         plt.savefig(f'imgs/{exp_name}/{exp_name}_mus_diff_{i}.png')
 
+def plot_kl(kls_all, exp_name, legend, config):
+
+    kls_all = np.array(kls_all)
+    os.makedirs(f'imgs/{exp_name}', exist_ok=True)
+    
+    print(kls_all.shape)
+    fig, ax = plt.subplots()
+    y = kls_all.mean(axis=1)
+    x = np.arange(0, y.shape[1], 1)/config['fit_per_epoch']
+    ci = kls_all.std(axis=1)*2
+
+    # REMOVE
+    for j in range(y.shape[0]):
+        y[j][y[j]<-.5] = 0
+        ax.plot(x,y[j])
+
+        # ax.fill_between(x, (y-ci)[:,i], (y+ci)[:,i], alpha=.3)
+
+    plt.hlines(config['eps'], 0, len(x), 'red', label='KL bound')
+    legend += ['KL bound']
+
+    ax.set_xlabel('epochs')
+    ax.set_ylabel('average KL')
+
+    plt.title(f'Average KL for lqr_dim {init_params["lqr_dim"]} n_ineff {init_params["n_ineff"]}')
+    plt.legend(legend)
+    plt.savefig(f'imgs/{exp_name}/{exp_name}_kls.png')
 
 if __name__ == '__main__':
 
-    exp_name = 'lqr_3_env_0_lqr_init'
-    exp_name = 'lqr_3_env_0'
-    exp_name = 'lqr_2'
-    exp_name = 'lqr_2_env_0'
-    exp_name = 'lqr_10_env_0_init_1e3'
-    exp_name = 'lqr_10_env_0'
-    exp_name = 'lqr_10_env_0_sig_1'
+    exp_name = 'lqr_dim_10_con'
+
     data_dir = os.path.join('logs', exp_name)
-    returns_mean_all, returns_std_all, optimal_reward_all, best_reward_all, mi_avg_all, mus_all, legend, init_params = load_data_from_dir(data_dir)
+    returns_mean_all, returns_std_all, optimal_reward_all, best_reward_all, mi_avg_all, mus_all, kls_all, legend, init_params = load_data_from_dir(data_dir)
     print('alg', legend)
     print('max min', np.array(returns_mean_all).max(), np.array(returns_mean_all).min())
     print('optimal_reward', np.array(optimal_reward_all).mean(axis=1))
@@ -364,10 +390,11 @@ if __name__ == '__main__':
     # returns_mean_all, returns_std_all, optimal_reward_all, best_reward_all, mi_avg_all, legend, init_params = load_data_from_dir_segway(data_dir)
     
     plot_data(returns_mean_all, returns_std_all, optimal_reward_all, legend, exp_name, init_params)
-    plot_mi(mi_avg_all, 1, exp_name, init_params)
+    # plot_mi(mi_avg_all, 1, exp_name, init_params)
 
-    plot_mu(mus_all, exp_name, init_params)
-    plot_mu_diff(mus_all, exp_name, init_params)
+    # plot_mu(mus_all, exp_name, init_params)
+    # plot_mu_diff(mus_all, exp_name, init_params)
+    plot_kl(kls_all, exp_name, legend, init_params)
 
     # for dim in [3, 5, 7]:
     #     exp_name = f'lqr_{dim}'
