7fe254f40b4fc2c868ff75f5add7ff58d949e58c
memmelma <marius.memmel@gmx.net>
2021-05-11 19:13:58 +0200

    fixed MI

--- mi.py
+++ mi.py
@@ -1,11 +1,11 @@
 import numpy as np
 np.random.seed(42)
 
+# closed form entropy as in REPS compendium 
 def entropy(sig):
     n = sig.shape[0]
-    return 0.5*np.linalg.slogdet(sig)[1] + (np.log(2*np.pi))/2 + n/2
+    return 0.5*np.linalg.slogdet(sig)[1] + (n*np.log(2*np.pi))/2 + n/2
 
-# TODO: only analytical for multivariate so far
 dim = 1
 
 # p(X)
@@ -22,38 +22,46 @@ mu_e = np.atleast_1d(np.zeros(dim))
 sig_e = np.atleast_2d(np.zeros((dim,dim))) * 1e-3
 
 # p(Y|X)
+# https://ssl2.cms.fu-berlin.de/ewi-psy/einrichtungen/arbeitsbereiche/computational_cogni_neurosc/PMFN/10-Gaussian-distributions.pdf eq. 10.20
 mu_y_x = A @ mu_x + mu_e
 sig_y_x = A @ sig_x @ A.T + sig_e
 
 # p(Y)
-# TODO: wrong
-const = 1/np.sqrt(np.linalg.det(2*np.pi*(sig_x + sig_y_x))) * np.exp(-1/2 * (mu_x - mu_y_x).T @ np.linalg.inv(sig_x + sig_y_x) @ (mu_x - mu_y_x))
-sig_y = np.linalg.inv( np.linalg.inv(sig_x) + np.linalg.inv(sig_y_x) ) * const
-# sig_y = ( np.linalg.inv(sig_x) + np.linalg.inv(sig_y_x) ) # * const
-# sig_y = sig_x@np.linalg.inv(sig_x + sig_y_x)@sig_y_x
+# Bishop p.93 eq. 2.115
+sig_y = sig_y_x + A@sig_x@A.T
+# p(X|Y)
+# Bishop p.93 eq. 2.116
+sig_x_y = np.linalg.inv(np.linalg.inv(sig_x) + A.T@np.linalg.inv(sig_y_x)@A)
 
 # p(X,Y)
 sig_xy = np.block( [ [sig_x,    sig_x@A.T], 
-                    [A@sig_x,   sig_y_x + A@sig_x@A.T + sig_e] ] )
+                    [A@sig_x,   sig_y_x + A@sig_x@A.T] ] )
     
 H_x = entropy(sig_x)
-H_y_x = entropy(sig_y_x)
 H_y = entropy(sig_y)
 H_xy = entropy(sig_xy)
-# TODO: H(y) >= H(y|x)
-print('H_x', H_x, 'H_y_x', H_y_x, 'H_y', H_y, 'H_xy', H_xy)
+H_y_x = entropy(sig_y_x)
+H_x_y = entropy(sig_x_y)
 
-# TODO: both should be equal
+print('H_x', H_x, 'H_y', H_y, 'H_y_x', H_y_x, 'H_x_y', H_x_y, 'H_xy', H_xy)
+
 I_xy = H_x + H_y - H_xy
-print('analytical', I_xy)
+print('H_x + H_y - H_xy', I_xy)
 I_xy = H_y - H_y_x
-print('analytical', I_xy)
+print('H_y - H_y_x', I_xy)
+I_xy = H_x - H_x_y
+print('H_x - H_x_y', I_xy)
+I_xy = H_xy - H_x_y - H_y_x
+print('H_xy - H_x_y - H_y_x', I_xy)
 
-n_samples = 1000
+n_samples = 500
 x = np.random.multivariate_normal(mu_x, sig_x, size=n_samples).T
 e = np.random.multivariate_normal(mu_e, sig_e, size=n_samples).T
 
 y = A@x + e
+
+# https://github.com/scikit-learn/scikit-learn/blob/15a949460/sklearn/feature_selection/_mutual_info.py#L291
+# uses NearestNeighbors and KDtree instead of contingency
 
 from sklearn.feature_selection import mutual_info_regression
 def calc_MI_sklearn_regression(x, y, bins):
@@ -81,4 +89,4 @@ I_xy_sklearn_regression = calc_MI_sklearn_regression(x.T, y.T, bins)
 I_xy_sklearn = calc_MI_sklearn(x.squeeze(), y.squeeze(), bins)
 I_xy_scipy = calc_MI_scipy(x.squeeze(), y.squeeze(), bins)
 
-print('sklearn_regression', I_xy_sklearn_regression, 'sklearn', I_xy_sklearn, 'scipy', I_xy_scipy)
+print('sklearn_regression', I_xy_sklearn_regression, '\nsklearn', I_xy_sklearn, '\nscipy', I_xy_scipy)
