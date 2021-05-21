9c5016256d89f46040a3cb5308028d5248bf92b8
memmelma <marius.memmel@gmx.net>
2021-05-21 16:22:32 +0200

    multi dim mi

--- mi.py
+++ mi.py
@@ -27,12 +27,29 @@ def calc_MI_scipy(x, y, bins):
     return mi
 
 
-from sklearn.metrics import mutual_info_score
+from sklearn.metrics import mutual_info_score, adjusted_mutual_info_score
 def calc_MI_sklearn(x, y, bins):
     c_xy = np.histogram2d(x, y, bins)[0]
     mi = mutual_info_score(None, None, contingency=c_xy)
     return mi
 
+def calc_MI(X,Y,bins,H_X_given):
+    c_XY = np.histogram2d(X,Y,bins)[0]
+    c_X = np.histogram(X,bins)[0]
+    c_Y = np.histogram(Y,bins)[0]
+
+    H_X = shan_entropy(c_X)
+    H_Y = shan_entropy(c_Y)
+    H_XY = shan_entropy(c_XY)
+    # print('H_X, H_X_given', H_X, H_X_given)
+    MI = H_X + H_Y - H_XY
+    return MI
+
+def shan_entropy(c):
+    c_normalized = c / float(np.sum(c))
+    c_normalized = c_normalized[np.nonzero(c_normalized)]
+    H = -sum(c_normalized* np.log2(c_normalized))  
+    return H
 
 def get_mi(noise_factor=1, dim=5, n_samples=1000, bins=[3,5]):
 
@@ -64,7 +81,7 @@ def get_mi(noise_factor=1, dim=5, n_samples=1000, bins=[3,5]):
     # p(X,Y)
     sig_xy = np.block( [ [sig_x,    sig_x@A.T], 
                         [A@sig_x,   sig_y_x + A@sig_x@A.T] ] )
-        
+    
     H_x = entropy(sig_x)
     H_y = entropy(sig_y)
     H_xy = entropy(sig_xy)
@@ -81,20 +98,21 @@ def get_mi(noise_factor=1, dim=5, n_samples=1000, bins=[3,5]):
     I = H_x + H_y - H_xy
     # print('H_x + H_y - H_xy', I)
 
-    
     assert np.all(np.linalg.eigvals(sig_x) > 0), 'sig_x not positive-definite'
     
-    assert np.all(np.linalg.eigvals(sig_e) > 0), 'sig_e not positive-definite'
+    if noise_factor > 0:
+        assert np.all(np.linalg.eigvals(sig_e) > 0), 'sig_e not positive-definite'
 
     x = np.random.multivariate_normal(mu_x, sig_x, size=n_samples).T
     e = np.random.multivariate_normal(mu_e, sig_e, size=n_samples).T
 
     # sample y values
     y = A@x + e
-    
+
     I_xy_sklearn_regression = 0
     I_xy_sklearn = []
     I_xy_scipy = 0
+    I_xy_samples = 0
 
     legend = []
 
@@ -110,13 +128,14 @@ def get_mi(noise_factor=1, dim=5, n_samples=1000, bins=[3,5]):
             else:
                 I_xy_sklearn[bin_i] += calc_MI_sklearn(x_tmp.squeeze(), y[i].squeeze(), bin)
 
-        I_xy_scipy += calc_MI_scipy(x_tmp.squeeze(), y[i].squeeze(), bins[0])
-        if i == 0:
-            legend += [f'I_xy_scipy_bins_{bins[0]}']
-
+        # I_xy_scipy += calc_MI_scipy(x_tmp.squeeze(), y[i].squeeze(), bins[0])
+        # I_xy_samples += calc_MI(x_tmp.squeeze(), y[i].squeeze(), bins[0], H_X_given=H_x)
+        # if i == 0:
+        #     legend += [f'I_xy_scipy_bins_{bins[0]}']
+        #     legend += [f'I_xy_samples_bins_{bins[0]}']
     legend += ['I']
 
-    return np.array([I_xy_sklearn_regression, *I_xy_sklearn, I_xy_scipy, I]), legend
+    return np.array([I_xy_sklearn_regression, *I_xy_sklearn, I_xy_scipy, I_xy_samples, I]), legend
 
 
 def plot_standard(dim = 5, n_samples = 500, bins = [3], noise_factors = np.arange(1, 10, 0.1), n_runs = 25):
@@ -149,12 +168,48 @@ def plot_standard(dim = 5, n_samples = 500, bins = [3], noise_factors = np.arang
     plt.title(f'dimensionality: {dim}, samples: {n_samples}, bins: {bins}')
     plt.savefig(os.path.join(img_dir, f'mi_dim_{dim}_samples_{n_samples}_bins_{bins}.png'))
 
+def plot_samples(dim = 5, n_samples = [500], bins = [3], noise_factor = 2, n_runs = 25):
+    mi_runs = []
+    
+    for n in n_samples:
+        np.random.seed(42)
+        mi_trial = []
+        for i in range(n_runs):
+            mi, legend = get_mi(noise_factor=noise_factor, dim=dim, n_samples=n, bins=bins)
+            mi_trial += [mi]
+        mi_runs += [mi_trial]
+
+    mi_runs_mean = np.array(mi_runs).mean(axis=1).T
+    mi_runs_std = np.array(mi_runs).std(axis=1).T
+
+    fig, ax = plt.subplots()
+
+    x = n_samples # np.arange(0, mi_runs_mean.shape[1], 1)
+
+    for mi_run_mean, mi_run_std, label in zip(mi_runs_mean, mi_runs_std, legend):
+        ax.plot(x, mi_run_mean, label=label)
+        ci = mi_run_std*2
+        ax.fill_between(x, mi_run_mean-ci, mi_run_mean+ci, alpha=.3)
+
+    # ax.legend(['I_xy_sklearn_regression', 'I_xy_sklearn', 'I_xy_scipy', 'I'])
+    ax.legend()
+    ax.set_xlabel('samples')
+    ax.set_ylabel('average mutual information')
+    plt.title(f'dimensionality: {dim}, samples: {min(n_samples), max(n_samples)}, bins: {bins}')
+    plt.savefig(os.path.join(img_dir, f'mi_dim_{dim}_noise_{noise_factor}_bins_{bins}.png'))
+
 if __name__ == '__main__':
 
     img_dir = 'mi_toy'
     os.makedirs(img_dir, exist_ok=True)
 
-    for dim in [5, 10]:
-        for n_samples in [25, 100]:
-            plot_standard(dim = dim, n_samples = n_samples, bins = [2, 3, 5], noise_factors = np.arange(1, 100, 1), n_runs=25)
+    n_runs = 10
+
+    # for dim in [5, 10]:
+    #     for n_samples in [25, 100]:
+    #         plot_standard(dim = dim, n_samples = n_samples, bins = [2, 3, 5], noise_factors = np.arange(1, 50, 1), n_runs=n_runs)
+            
+    for dim in [1, 5, 10]:
+        for noise_factor in [0, 5, 10]:
+            plot_samples(dim = dim, n_samples = np.arange(5, 250, 5), bins = [2, 3, 5, 10], noise_factor = noise_factor, n_runs=n_runs)
     
