ed84833a9a9489bf84b4f4d4d7117db9ea8f97cc
memmelma <marius.memmel@gmx.net>
2021-06-10 16:55:38 +0200

    refactor and plot fix

--- mi.py
+++ mi.py
@@ -1,49 +1,44 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.lines import Line2D
+
+from sklearn import neighbors
+from sklearn.metrics import mutual_info_score
+from sklearn.feature_selection import mutual_info_regression
 
 import knnie
 
-# closed form entropy as in REPS compendium 
 def entropy(sig):
+	# closed form entropy as in REPS compendium 
 	n = sig.shape[0]
-
-	# return 0.5*np.log2(np.linalg.det(sig)) + (n*np.log2(2*np.pi))/2 + n/2
 	return 0.5*np.linalg.slogdet(sig)[1] + (n*np.log(2*np.pi))/2 + n/2
 
-
-# https://github.com/scikit-learn/scikit-learn/blob/15a949460/sklearn/feature_selection/_mutual_info.py#L291
-# uses NearestNeighbors and KDtree instead of contingency
-
-from sklearn.feature_selection import mutual_info_regression
 def calc_MI_sklearn_regression(x, y, n_neighbors=3):
-	mi = mutual_info_regression(x,y, n_neighbors=n_neighbors)
-	# mi = knnie.kraskov_mi(np.atleast_2d(x),np.atleast_2d(y).T, k=n_neighbors)
+	# KSG implementation of sklearn
+	# https://github.com/scikit-learn/scikit-learn/blob/15a949460/sklearn/feature_selection/_mutual_info.py#L291
+	# Same except preprocessing (scaling of X, noise to X)
+	# mi = mutual_info_regression(x, y, n_neighbors=n_neighbors, discrete_features=False)
+	mi = knnie._estimate_mi(x, y.ravel(), n_neighbors=n_neighbors, discrete_features=False, discrete_target=False)
+	return mi[0]
 
-	return float(mi.squeeze())
-
-
-# https://stackoverflow.com/a/20505476
-
-from scipy.stats import chi2_contingency
-def calc_MI_scipy(x, y, bins):
-	# mi = knnie.kraskov_mi(np.atleast_2d(x).T,np.atleast_2d(y).T, k=3)
-	# mi = knnie.revised_mi(np.atleast_2d(x).T,np.atleast_2d(y).T, k=3)
-
-	c_xy = np.histogram2d(x, y, bins)[0]
-	g, p, dof, expected = chi2_contingency(c_xy, lambda_="log-likelihood")
-	mi = 0.5 * g / c_xy.sum()
-
+def calc_MI_KSG(x, y, n_neighbors=3):
+	# KSG
+	# mi = knnie.kraskov_mi(x, y, k=n_neighbors)
+	# KSG from sklearn
+	mi = knnie.kraskov_mi_sklearn(x, y, n_neighbors=n_neighbors)
 	return mi
 
-
-from sklearn.metrics import mutual_info_score, adjusted_mutual_info_score
 def calc_MI_sklearn(x, y, bins):
-	c_xy = np.histogram2d(x, y, bins)[0]
+	# https://stackoverflow.com/a/20505476
+	c_xy = np.histogram2d(x.ravel(), y.ravel(), bins)[0]
 	mi = mutual_info_score(None, None, contingency=c_xy)
 	return mi
 
-def calc_MI(x, y, bins, H_X_given, n):
+def calc_MI_samples(x, y, bins, H_X_given, n):
+	
+	x = x.ravel()
+	y = y.ravel()
 	
 	c_XY = np.histogram2d(x, y, bins)[0]
 	c_X = np.histogram(x, bins)[0]
@@ -62,7 +57,7 @@ def shan_entropy(c):
 	H = -sum(c_normalized* np.log(c_normalized))  
 	return H
 
-def get_mi(noise_factor=1, dim=5, n_samples=1000, bins=[3,5]):
+def analytical_MI(noise_factor, dim, n_samples):
 
 	# p(X)
 	mu_x = np.atleast_1d(np.random.rand(dim))
@@ -73,9 +68,8 @@ def get_mi(noise_factor=1, dim=5, n_samples=1000, bins=[3,5]):
 	# A = np.atleast_2d(np.random.rand(dim,dim))
 	
 	# noise distribution E
-	mu_e = np.atleast_1d(np.random.rand(1)) * noise_factor
 	mu_e = np.atleast_1d(np.zeros(1))
-	sig_e = np.atleast_2d(np.random.rand(1)) * noise_factor 
+	sig_e = np.atleast_2d(np.ones(1)) * noise_factor 
 	# sig_e = np.atleast_2d(np.random.rand(dim,dim))
 	# sig_e = sig_e @ sig_e.T * noise_factor
 
@@ -109,149 +103,140 @@ def get_mi(noise_factor=1, dim=5, n_samples=1000, bins=[3,5]):
 	I_2 = H_xy - H_x_y - H_y_x
 	I_3 = H_x + H_y - H_xy
 
-	# print('entropy analytical:', H_x, H_y, H_xy)
-
-	I = I_0
-
 	assert np.all(np.linalg.eigvals(sig_x) > 0), 'sig_x not positive-definite'
 	
 	if noise_factor > 0:
 		assert np.all(np.linalg.eigvals(sig_e) > 0), 'sig_e not positive-definite'
 
-	x = np.random.multivariate_normal(mu_x, sig_x, size=n_samples).T
-	e = np.random.multivariate_normal(mu_e, sig_e, size=n_samples).T
+	x = np.random.multivariate_normal(mu_x, sig_x, size=n_samples)
+	e = np.random.multivariate_normal(mu_e, sig_e, size=n_samples)
 
 	# sample y values
-	y = A@x + e
+	y = x@A.T + e
+
+	return x, y, I_3, H_x
+
+def compute_MI(x, y, I, H_x, bins):
 
 	I_xy_sklearn_regression = []
 	I_xy_sklearn = []
-	I_xy_scipy = 0
-	# I_xy_samples = 0
+	I_xy_scipy = []
 	I_xy_samples = []
 
 	legend = []
 
-		
-	for i in range(x.shape[0]):
-		x_tmp = np.atleast_2d(x[i,:]).T
-		y_tmp = y.T.squeeze()
-		
-		# for bin_i, bin in enumerate(bins):
-		#	 if i == 0:
-		#		 I_xy_sklearn_regression += [calc_MI_sklearn_regression(x_tmp, y_tmp, n_neighbors=bin)]
-		#		 legend += [f'I_xy_sklearn_regression_bins_{bin}']
-		#	 else:
-		#		 I_xy_sklearn_regression[bin_i] += calc_MI_sklearn_regression(x_tmp, y_tmp, n_neighbors=bin)
+	for i, x_i in enumerate(x.T):
 
-		# for bin_i, bin in enumerate(bins):
-		#	 if i == 0:
-		#		 I_xy_sklearn += [calc_MI_sklearn(x_tmp.squeeze(), y_tmp, bin)]
-		#		 legend += [f'I_xy_sklearn_bins_{bin}']
-		#	 else:
-		#		 I_xy_sklearn[bin_i] += calc_MI_sklearn(x_tmp.squeeze(), y_tmp, bin)
+		x_tmp = x_i[:,np.newaxis] # N*d_x
+		y_tmp = y # N*d_y -> N*1
+
+		for bin_i, bin in enumerate(bins):
+			 if i == 0:
+				 I_xy_sklearn_regression += [calc_MI_sklearn_regression(x_tmp, y_tmp, n_neighbors=bin)]
+			 else:
+				 I_xy_sklearn_regression[bin_i] += calc_MI_sklearn_regression(x_tmp, y_tmp, n_neighbors=bin)
 
 		for bin_i, bin in enumerate(bins):
 			if i == 0:
-				I_xy_samples += [calc_MI(x_tmp.squeeze(), y_tmp, bin, H_X_given=H_x, n=x.shape[0])]
-				legend += [f'I_xy_samples_bins_{bin}']
+				I_xy_sklearn += [calc_MI_sklearn(x_tmp, y_tmp, bin)]
 			else:
-				I_xy_samples[bin_i] += calc_MI(x_tmp.squeeze(), y_tmp, bin, H_X_given=H_x, n=x.shape[0])
+				I_xy_sklearn[bin_i] += calc_MI_sklearn(x_tmp, y_tmp, bin)
 
-		I_xy_scipy += calc_MI_scipy(x_tmp.squeeze(), y_tmp, bins[0])
-		if i == 0:
-			#  legend += [f'I_xy_scipy_bins_{bins[0]}']
-			 legend += [f'I_xy_knnie_bins_{bins[0]}']
+		for bin_i, bin in enumerate(bins):
+			if i == 0:
+				I_xy_samples += [calc_MI_samples(x_tmp, y_tmp, bin, H_X_given=H_x, n=x.shape[0])]
+			else:
+				I_xy_samples[bin_i] += calc_MI_samples(x_tmp, y_tmp, bin, H_X_given=H_x, n=x.shape[0])
 
-	legend += ['I']
+		for bin_i, bin in enumerate(bins):
+			if i == 0:
+				I_xy_scipy += [calc_MI_KSG(x_tmp, y_tmp, bin)]
+			else:
+				I_xy_scipy[bin_i] += calc_MI_KSG(x_tmp, y_tmp, bin)
+
+
+	legend += ['$I_{regress}$']
+	legend += ['$I_{binned}$']
+	legend += ['$I_{prior}$']
+	legend += ['$I_{KSG}$']
+
+	legend += ['$I$']
 
 	for i in range(len(bins)):
-		# I_xy_sklearn[i] /= x.shape[0]
-		# I_xy_sklearn_regression[i] /= x.shape[0]
+		I_xy_sklearn[i] /= x.shape[0]
+		I_xy_sklearn_regression[i] /= x.shape[0]
 		I_xy_samples[i] /= x.shape[0]
-	
-	I_xy_scipy /= x.shape[0]
-	# I_xy_samples /= x.shape[0]
-	
-	return np.array([*I_xy_samples, I_xy_scipy, I]), legend
+		I_xy_scipy[i] /= x.shape[0]
+
+	colors = ['tab:blue', 'tab:orange', 'tab:green', 'tab:purple', 'tab:red']
+	linestyles = [':', '-.', '--']
+	assert len(linestyles) == len(bins), 'define more linestyles'
+
+	return [*I_xy_sklearn_regression, *I_xy_sklearn, *I_xy_samples, *I_xy_scipy, I], legend, colors, linestyles
 	# return np.array([*I_xy_sklearn_regression, *I_xy_sklearn, I_xy_scipy, I_xy_samples, I]), legend
 
-
-def plot_standard(dim = 5, n_samples = 500, bins = [3], noise_factors = np.arange(1, 10, 0.1), n_runs = 25):
+def plot_samples(dim=5, n_samples=[5, 10, 15], bins=[3, 4], noise_factor=2, n_runs=25, override=False, log_dir=''):
 	mi_runs = []
 	
-	for noise in noise_factors:
-		np.random.seed(42)
-		mi_trial = []
-		for i in range(n_runs):
-			mi, legend = get_mi(noise_factor=noise, dim=dim, n_samples=n_samples, bins=bins)
-			mi_trial += [mi]
-		mi_runs += [mi_trial]
+	file_name = f'mi_dim_{dim}_noise_{noise_factor}_bins_{bins}'
+
+	if os.path.isfile(file_name) and not override: 
+		mi_runs, legend, colors, linestyle = np.load(os.path.join(log_dir, 'data', file_name+'.npy'), allow_pickle=True)
+		print('Open existing file')
+	else:
+		for n in n_samples:
+			np.random.seed(42)
+			mi_trial = []
+			for i in range(n_runs):
+				x, y , I, H_x = analytical_MI(noise_factor, dim, n)
+				mi, legend, colors, linestyle = compute_MI(x, y, I, H_x, bins)
+				mi_trial += [mi]
+			mi_runs += [mi_trial]
+		np.save(open(os.path.join(log_dir, 'data', file_name+'.npy'), 'wb'), (mi_runs, legend, colors, linestyle))
 
 	mi_runs_mean = np.array(mi_runs).mean(axis=1).T
+
 	mi_runs_std = np.array(mi_runs).std(axis=1).T
 
 	fig, ax = plt.subplots()
 
-	x = noise_factors # np.arange(0, mi_runs_mean.shape[1], 1)
+	x = n_samples
 
-	for mi_run_mean, mi_run_std in zip(mi_runs_mean, mi_runs_std):
-		ax.plot(x, mi_run_mean)
+	for i, (mi_run_mean, mi_run_std) in enumerate(zip(mi_runs_mean[:-1], mi_runs_std[:-1])):
+		ax.plot(x, mi_run_mean, color=colors[:-1][i//(len(bins))], linestyle=linestyle[i%(len(bins))])
 		ci = mi_run_std*2
-		ax.fill_between(x, mi_run_mean-ci, mi_run_mean+ci, alpha=.3)
+		ax.fill_between(x, mi_run_mean-ci, mi_run_mean+ci, alpha=.2, color=colors[:-1][i//(len(bins))])
 
-	# ax.legend(['I_xy_sklearn_regression', 'I_xy_sklearn', 'I_xy_scipy', 'I'])
-	ax.legend(legend)
-	ax.set_xlabel('noise')
-	ax.set_ylabel('average mutual information')
-	plt.title(f'dimensionality: {dim}, samples: {n_samples}, bins: {bins}')
-	plt.savefig(os.path.join(img_dir, f'mi_dim_{dim}_samples_{n_samples}_bins_{bins}.png'))
+	ax.plot(x, mi_runs_mean[-1], color=colors[-1], linestyle='-')
 
-def plot_samples(dim = 5, n_samples = [500], bins = [3], noise_factor = 2, n_runs = 25):
-	mi_runs = []
-	
-	for n in n_samples:
-		np.random.seed(42)
-		mi_trial = []
-		for i in range(n_runs):
-			mi, legend = get_mi(noise_factor=noise_factor, dim=dim, n_samples=n, bins=bins)
-			mi_trial += [mi]
-		mi_runs += [mi_trial]
+	legend_elements = []
+	for color, leg in zip(colors, legend):
+		legend_elements += [Line2D([0], [0], color=color, lw=1, label=leg)]
 
-	mi_runs_mean = np.array(mi_runs).mean(axis=1).T
-	mi_runs_std = np.array(mi_runs).std(axis=1).T
+	for i, bin in enumerate(bins):
+		legend_elements += [Line2D([0], [0], color='black', lw=1, label=f'bins/$k$={bin}', linestyle=linestyle[i])]
 
-	fig, ax = plt.subplots()
 
-	x = n_samples # np.arange(0, mi_runs_mean.shape[1], 1)
+	ax.legend(handles=legend_elements)
 
-	for mi_run_mean, mi_run_std, label in zip(mi_runs_mean, mi_runs_std, legend):
-		ax.plot(x, mi_run_mean, label=label)
-		ci = mi_run_std*2
-		ax.fill_between(x, mi_run_mean-ci, mi_run_mean+ci, alpha=.3)
-
-	# ax.legend(['I_xy_sklearn_regression', 'I_xy_sklearn', 'I_xy_scipy', 'I'])
-	ax.legend()
 	ax.set_xlabel('samples')
-	ax.set_ylabel('average mutual information')
-	plt.title(f'dimensionality: {dim}, samples: {min(n_samples), max(n_samples)}, bins: {bins}')
-	plt.savefig(os.path.join(img_dir, f'mi_dim_{dim}_noise_{noise_factor}_bins_{bins}.png'))
+	ax.set_ylabel('mutual information')
+	plt.title(f'dimensionality: {dim}')
+
+	plt.savefig(os.path.join(log_dir, 'imgs', file_name+'.pdf'))
+	plt.savefig(os.path.join(log_dir, 'imgs', file_name+'.png'))
 
 if __name__ == '__main__':
 
-	img_dir = 'mi_toy'
+	log_dir = 'logs_mi'
+	img_dir = os.path.join(log_dir, 'imgs')
+	data_dir = os.path.join(log_dir, 'data')
+	os.makedirs(log_dir, exist_ok=True)
 	os.makedirs(img_dir, exist_ok=True)
+	os.makedirs(data_dir, exist_ok=True)
 
-	# plot_samples(dim=140, n_samples=np.arange(15, 250, 5), bins=[3, 5, 10], noise_factor=0, n_runs=10)
-	plot_samples(dim=50, n_samples=np.arange(10, 250, 5), bins=[3, 5], noise_factor=0, n_runs=10)
+	plot_samples(dim=140, n_samples=np.arange(10, 250, 5), bins=[3, 4, 5], noise_factor=0, n_runs=10, log_dir=log_dir)
 
-	# n_runs = 10
+	# plot_samples(dim=140, n_samples=np.arange(10, 250, 5), bins=[3, 4, 5], noise_factor=1, n_runs=10, log_dir=log_dir)
 
-	# # for dim in [5, 10]:
-	# #	 for n_samples in [25, 100]:
-	# #		 plot_standard(dim = dim, n_samples = n_samples, bins = [2, 3, 5], noise_factors = np.arange(1, 50, 1), n_runs=n_runs)
-			
-	# for dim in [1, 5, 10]:
-	#	 for noise_factor in [0, 5, 10]:
-			# plot_samples(dim = dim, n_samples = np.arange(5, 250, 5), bins = [2, 3, 5, 10], noise_factor = noise_factor, n_runs=n_runs)
 	
