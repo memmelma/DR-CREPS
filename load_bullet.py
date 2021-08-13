d0e35801da28cada96e23416fae1a2575ef7b584
memmelma <marius.memmel@gmx.net>
2021-08-13 11:00:39 +0200

    added loading for bullet policies

---
+++ load_bullet.py
@@ -0,0 +1,20 @@
+import joblib
+from el_bullet_mi import experiment
+
+if __name__ == '__main__':
+    
+    exp_name = 'bullet_reps_mi/alg_REPS_MI/eps_0.9/method_MI/sample_type_percentage/gamma_0.9/k_100/distribution_diag'
+    load_exp = 'REPS_MI_1'
+
+    # exp_name = 'bullet_reps_mi/alg_REPS/eps_0.005/distribution_diag'
+    # load_exp = 'REPS_0'
+
+    params = joblib.load(f'logs/{exp_name}/{load_exp}')['init_params']
+    state = joblib.load(f'logs/{exp_name}/{load_exp}_state')['distribution']
+
+    params['sigma_init'] = state
+    params['ep_per_fit'] = 1
+    params['quiet'] = False
+    
+    print(params)
+    experiment(**params)
