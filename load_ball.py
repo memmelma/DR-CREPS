cb8b6eed1d14e9047286b54790c0b4cfef60eee1
memmelma <marius.memmel@gmx.net>
2021-08-13 11:00:30 +0200

    fixed imports

--- load_ball.py
+++ load_ball.py
@@ -1,17 +1,18 @@
 import joblib
-from ball_mi_el import experiment
+from el_ball_mi import experiment
 
 if __name__ == '__main__':
     
-    exp_name = 'ball'
+    exp_name = 'ball_eps/alg_ConstrainedREPSMI/eps_0.7/k_25/gamma_0.1/kappa_3.0'
+    exp_name = 'ball_eps/alg_ConstrainedREPS/eps_2.2/kappa_8.0'
     load_exp = 'ConstrainedREPSMI_0'
+    load_exp = 'ConstrainedREPS_0'
 
     params = joblib.load(f'logs/{exp_name}/{load_exp}')['init_params']
     state = joblib.load(f'logs/{exp_name}/{load_exp}_state')['distribution']
 
     params['sigma_init'] = state
     params['quiet'] = False
-
-    experiment(**params)
-
     
+    print(params)
+    experiment(**params)
