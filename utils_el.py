1ca20382cb57d56aa08cda2efc56dc7fb5f26fd5
memmelma <marius.memmel@gmx.net>
2021-10-21 14:04:40 +0200

    added rwr mi

--- utils_el.py
+++ utils_el.py
@@ -5,6 +5,7 @@ from custom_distributions.gaussian_custom import GaussianDiagonalDistribution, G
 from mushroom_rl.algorithms.policy_search.black_box_optimization import REPS, RWR
 from custom_algorithms.more import MORE
 from custom_algorithms.reps_mi import REPS_MI
+from custom_algorithms.rwr_mi import RWR_MI
 from custom_algorithms.constrained_reps import ConstrainedREPS
 from custom_algorithms.constrained_reps_mi import ConstrainedREPSMI
 
@@ -15,7 +16,6 @@ def init_distribution(mu_init=0, sigma_init=1e-3, size=1, sample_type=None, gamm
     
     mu = mu_init * np.ones(size)
 
-    
     if distribution_class == 'diag':
 
         # load full covariance
@@ -83,6 +83,10 @@ def init_algorithm(algorithm_class='REPS', params={}):
     elif algorithm_class == 'RWR':
         alg = RWR
         params = {'beta': params['eps']}
+    
+    elif algorithm_class == 'RWR_MI':
+        alg = RWR_MI
+        params = {'eps': params['eps'], 'gamma': params['gamma'], 'k': params['k'], 'bins': params['bins'], 'method': params['method'], 'mi_type': params['mi_type'], 'mi_avg': params['mi_avg']}
 
     elif algorithm_class == 'MORE':
         alg = MORE
