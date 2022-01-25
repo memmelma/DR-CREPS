c34828e0a021c2ff4e525f555089ea2d4828c3cd
memmelma <marius.memmel@gmx.net>
2022-01-25 13:04:07 +0100

    rework run file

--- run.py
+++ run.py
@@ -7,25 +7,69 @@ if __name__ == '__main__':
     local = False
     test = False
 
+    # LQR
     experiment_name = f'LQR_camera_ready'
-    # experiment_name = f'Ball_Stopping_camera_ready'
-
     launcher = Launcher(experiment_name,
                         'experiment_config',
-                        1,
-                        memory=3000,
+                        25,
+                        memory=500,
                         days=2,
-                        hours=0,
-                        minutes=0,
-                        seconds=0,
                         use_timestamp=False,
                         conda_env='iprl'
                         )
     
-    # launcher = reproduce_lqr_experiments(launcher) # memory=500
-    launcher = reproduce_ship_steering(launcher) # memory=3000
-    # launcher = reproduce_air_hockey(launcher) # memory=1000
-    # launcher = reproduce_ball_stopping(launcher) # memory=3000
+    launcher = reproduce_lqr_experiments(launcher)
+    
+    print('name:', experiment_name)
+    print('experiments:', len(launcher._experiment_list))
+    launcher.run(local, test)
+
+
+    # ShipSteering
+    experiment_name = f'BallStopping_camera_ready'
+    launcher = Launcher(experiment_name,
+                        'experiment_config',
+                        25,
+                        memory=3000,
+                        days=2,
+                        use_timestamp=False,
+                        conda_env='iprl'
+                        )
+    launcher = reproduce_ship_steering(launcher)
+
+    print('name:', experiment_name)
+    print('experiments:', len(launcher._experiment_list))
+    launcher.run(local, test)
+
+
+    # AirHockey
+    experiment_name = f'AirHockey_camera_ready'
+    launcher = Launcher(experiment_name,
+                        'experiment_config',
+                        25,
+                        memory=1000,
+                        days=2,
+                        use_timestamp=False,
+                        conda_env='iprl'
+                        )
+    launcher = reproduce_air_hockey(launcher)
+
+    print('name:', experiment_name)
+    print('experiments:', len(launcher._experiment_list))
+    launcher.run(local, test)
+
+
+    # BallStopping
+    experiment_name = f'BallStopping_camera_ready'
+    launcher = Launcher(experiment_name,
+                        'experiment_config',
+                        25,
+                        memory=3000,
+                        days=2,
+                        use_timestamp=False,
+                        conda_env='iprl'
+                        )
+    launcher = reproduce_ball_stopping(launcher)
 
     print('name:', experiment_name)
     print('experiments:', len(launcher._experiment_list))
