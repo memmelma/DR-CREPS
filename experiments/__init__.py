1f88f2af7e17d90d25158c77538c521b749ee74e
memmelma <marius.memmel@gmx.net>
2022-01-30 16:50:43 +0100

    added LQR diag

--- experiments/__init__.py
+++ experiments/__init__.py
@@ -1,2 +1,2 @@
 from .utils import *
-from .reproduce_experiments import reproduce_lqr_experiments, reproduce_ship_steering, reproduce_air_hockey, reproduce_ball_stopping
+from .reproduce_experiments import reproduce_diag_lqr_experiments, reproduce_lqr_experiments, reproduce_ship_steering, reproduce_air_hockey, reproduce_ball_stopping
